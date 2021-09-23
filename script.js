a69e5b324124669af02aa9bfd9fa2b933ccf87db
Alfred <alfred.b.wong@gmail.com>
2021-09-23 16:02:28 -0400

    Add additional styling to better match requirements

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function compute() {
       principal +
       "</span>,<br>at an interest rate of <span class='highlight'>" +
       rate +
-      "%</span><br>You will receive an amount of <span class='highlight'>" +
+      "%.</span><br>You will receive an amount of <span class='highlight'>" +
       interest +
       "</span>,<br>in the year <span class='highlight'>" +
       year +
